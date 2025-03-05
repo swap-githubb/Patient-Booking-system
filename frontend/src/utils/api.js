282e081a84ff0bf5c13053599e729694deeef263
swap-githubb <125266256+swap-githubb@users.noreply.github.com>
2025-03-05 21:48:11 +0530

    Updated api.js with deployed backend link

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api' // Update this URL once your backend is deployed.
+  baseURL: 'https://patient-booking-system.onrender.com' // Update this URL once your backend is deployed.
 });
 
 export default api;
